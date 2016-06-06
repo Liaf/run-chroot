0602af7c488f18da0b80b2ad38a6627bc0ef952a
Liaf <liaf@lizard>
2016-06-06 15:53:25 +0200

    copy binary into chroot and execute it

--- run-in-chroot.c
+++ run-in-chroot.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 
@@ -11,16 +10,32 @@ int main(int argc, char *argv[]){
 		printf("Usage: %s <program name>\n", argv[0]);
 		exit(0);
 	}
+	
+	int binary;
+	int binaryInChroot;
+	char c[1];
+
 	// create directory
 	mkdir("./myroot", 0777);
+
 	// create filesystem
 	system("debootstrap sid ./myroot");
-	// touch
-	open("./myroot/run/in-chroot", O_CREAT, 007);
+
+	// move program you want to execute
+	if((binary = open(argv[1], O_RDONLY)) < 0) printf("Cannot open binary.\n");
+	if((binaryInChroot = open("./myroot/testThis", O_CREAT | O_WRONLY, 0700)) < 0) printf("Cannot open to write.\n");
+	while((read(binary, &c, 1)) > 0){
+		write(binaryInChroot, &c, 1);
+	}
+	close(binary);
+	close(binaryInChroot);
+
 	// chroot
 	chroot("myroot");
-	execve(argv[1], &argv[1], NULL);
+	chdir("/");
 
+	// execute program
+	system("/testThis");
 
 	return 0;
 }
